8e4b741ce4e3b523e9c742059c6394f1fa914982
kgathola <77323146+kgatholapuka@users.noreply.github.com>
2022-07-19 19:46:58 +0300

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -113,13 +113,15 @@ def main():
 		
 		# You can read a markdown file from supporting resources folder
 		st.markdown("""
-		Our data engineering team pulled the data from Twitter. After sending data to data 
- Science Team Once most of the data has been cleaned, what is  the procedure to repair or remove incorrect, corrupted, misformatted, 
- duplicate or incomplete data in the data set.
-  We work with 15819 rows of data and 3 columns. We have four different labels
+		The collection of this data was funded by a Canada Foundation for Innovation JELF Grant to Chris Bauch, University of Waterloo.
+This dataset aggregates tweets pertaining to climate change collected between Apr 27, 2015 and Feb 21, 2018. In total, 43943 tweets were annotated. 
+Each tweet is labelled independently by 3 reviewers. 
+This dataset only contains tweets that all 3 reviewers agreed on (the rest were discarded).
+    Each tweet is labelled as one of the following classes:
 		""")
 
 		st.markdown("""
+		
 		* 2 News: the tweet links to factual news about climate change
 * 1 Pro: the tweet supports the belief of man-made climate change
 * 0 Neutral: the tweet neither supports nor refutes the belief of man-made climate change
