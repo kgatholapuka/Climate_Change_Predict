b659956366c7e82b60dedae33046f145bab25404
kgathola <77323146+kgatholapuka@users.noreply.github.com>
2022-07-19 19:41:59 +0300

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -119,12 +119,12 @@ def main():
   We work with 15819 rows of data and 3 columns. We have four different labels
 		""")
 
-		st.markdown("2(News): the tweet links to factual news about climate change"
-
-		"1(Pro): the tweet supports the belief of man-made climate change"
-
-		"0(Neutral): the tweet neither supports nor refutes the belief of man-made climate change"
-		"-1(Anti): the tweet does not believe in man-made climate change")
+		st.markdown("""
+		* 2 News: the tweet links to factual news about climate change
+* 1 Pro: the tweet supports the belief of man-made climate change
+* 0 Neutral: the tweet neither supports nor refutes the belief of man-made climate change
+* -1 Anti: the tweet does not believe in man-made climate change
+		""")
        
 		st.markdown("Below is raw data that we used for Bulding the model")
 
