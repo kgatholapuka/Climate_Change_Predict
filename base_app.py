40e4ddfac0e08f51aac60fa6be5bd791df7a14fe
kgathola <77323146+kgatholapuka@users.noreply.github.com>
2022-07-20 22:48:59 +0300

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -33,6 +33,7 @@ nlp = NLP()
 
 import nltk
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 image = Image.open('img/3.png')
 st.set_page_config(page_title='Networkers.net',page_icon = image)
 
